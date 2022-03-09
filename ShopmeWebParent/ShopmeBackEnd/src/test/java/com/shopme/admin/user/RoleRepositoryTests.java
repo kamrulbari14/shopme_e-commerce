e7ad99008ff704976cd991c8c46d4b6fddba61f8
kamrulbari14 <kamrulbpriyo0523@gmail.com>
2022-03-10 00:40:32 +0600

    database connectivity and test

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
@@ -0,0 +1,25 @@
+package com.shopme.admin.user;
+
+import com.shopme.common.entity.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class RoleRepositoryTests {
+
+    @Autowired
+    private RoleRepository repo;
+
+    @Test
+    public void testCreateFirstRole() {
+        Role roleAdmin = new Role("Admin", "Manages everything");
+        Role saveRole = repo.save(roleAdmin);
+        assertThat(saveRole.getId()).isGreaterThan(0);
+
+    }
+}
